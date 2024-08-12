9324499cf29b4e32a3f5e95b3f6fd9f7a5881ce0
aish8la <64914670+aish8la@users.noreply.github.com>
2024-08-13 01:45:50 +0500

    change to button style selector

--- javascript.js
+++ javascript.js
@@ -1,1 +1,7 @@
-const addNewButton = document.querySelector()
+const addNewButton = document.querySelector('#add-new-btn');
+const formDialogue = document.querySelector('form-dialogue');
+
+addNewButton.addEventListener('click', () => {
+    formDialogue.showModal();
+});
+
