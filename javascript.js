feb3d0057ebba0976b84b7afb1d0bcbe9193b0a7
aish8la <64914670+aish8la@users.noreply.github.com>
2024-08-13 02:29:29 +0500

    add close button feature for dialogue

--- javascript.js
+++ javascript.js
@@ -1,7 +1,11 @@
 const addNewButton = document.querySelector('.new-button');
 const formDialogue = document.querySelector('.form-dialogue');
+const formCloseButton = document.querySelector('.form-close-button');
 
 addNewButton.addEventListener('click', () => {
     formDialogue.showModal();
 });
 
+formCloseButton.addEventListener('click', () => {
+    formDialogue.close();
+});
