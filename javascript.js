9545e91cd1da3b43fb85c3b21aaa6ab0f5eaecd0
aish8la <64914670+aish8la@users.noreply.github.com>
2024-08-14 23:23:54 +0500

    add library array and book object constructor

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
 const addNewButton = document.querySelector('.new-button');
 const formDialogue = document.querySelector('.form-dialogue');
 const formCloseButton = document.querySelector('.form-close-button');
+const myLibrary = [];
 
 addNewButton.addEventListener('click', () => {
     formDialogue.showModal();
@@ -9,3 +10,10 @@ addNewButton.addEventListener('click', () => {
 formCloseButton.addEventListener('click', () => {
     formDialogue.close();
 });
+
+function BookObject(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
